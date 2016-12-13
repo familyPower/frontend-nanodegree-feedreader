d59b08b042875e232d0f6a90f1edd82a54c5175e
Smitty <stobor2004@gmail.com>
2016-12-13 16:39:18 -0500

    Add before code to call ajax function.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -103,34 +103,47 @@ $( function() {
   /* TODO: Write a new test suite named "Initial Entries" */
   describe( 'Initial Entries', function() {
 
-    //         // run the async call before each test
-    //         // beforeEach(function() {
-    //         //     // make the jquery async call.
-    //         //     var loadFeedMock = spyOn(loadFeed);
-    //         //     spyOn(fakeElement, 'click').and.callThrough();
-    //         // });
-    //
-    //         /*
-    //          * TODO: Write a test that ensures when the loadFeed
-    //          * function is called and completes its work, there is at least
-    //          * a single .entry element within the .feed container.
-    //          * Remember, loadFeed() is asynchronous so this test will require
-    //          * the use of Jasmine's beforeEach and asynchronous done() function.
-    //          */
-    //
-    //
-    //
-    //         /* TODO: Write a new test suite named "New Feed Selection"
-    //     //     describe('New Feed Selection', function() {
-    //     //
-    //     //         /* TODO: Write a test that ensures when a new feed is loaded
-    //     //          * by the loadFeed function that the content actually changes.
-    //     //          * Remember, loadFeed() is asynchronous.
-    //     //          */
-    //     // });
-    // });
+    // run the async call before each test
+    console.log( "here-start" );
+    beforeEach( function() {
+      $.ajax( {
+        url: 'index.html',
+        data: [],
+        success: function( data ) {
+          setFixtures( data );
+          successCallback = ( function( loadFeed ) {
+            return function( data ) {
+              loadFeed( 0 );
+              done();
+            };
+          } )( successCallback );
+        },
+        dataType: 'html'
+      } );
+      console.log( "here-end" );
+    } );
 
+    /*
+     * TODO: Write a test that ensures when the loadFeed
+     * function is called and completes its work, there is at least
+     * a single .entry element within the .feed container.
+     * Remember, loadFeed() is asynchronous so this test will require
+     * the use of Jasmine's beforeEach and asynchronous done() function.
+     */
+
+
+
+    /* TODO: Write a new test suite named "New Feed Selection"
+        //     describe('New Feed Selection', function() {
+        //
+        //         /* TODO: Write a test that ensures when a new feed is loaded
+        //          * by the loadFeed function that the content actually changes.
+        //          * Remember, loadFeed() is asynchronous.
+        //          */
+    // });
   } );
+
+  // } );
 
 
 }() );
